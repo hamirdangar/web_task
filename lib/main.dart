9ddae6fbcc778999499c642b47b8660db0b837c7
hamir dangar <dangarhamir3333@gmail.com>
2025-02-11 10:17:47 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,167 @@
+import 'dart:html' as html;
+import 'dart:ui_web' as ui;
 import 'package:flutter/material.dart';
 
 void main() {
+  ui.platformViewRegistry.registerViewFactory(
+    'imageElement',
+        (int viewId) {
+      final img = html.ImageElement()
+        ..id = 'imageElement'
+        ..style.width = 'auto'
+        ..style.height = 'auto'
+        ..style.maxWidth = '80%'
+        ..style.maxHeight = '80%'
+        ..style.position = 'absolute'
+        ..style.left = '50%'
+        ..style.top = '50%'
+        ..style.transform = 'translate(-50%, -50%)';
+      return img;
+    },
+  );
+
   runApp(const MyApp());
 }
 
+/// Application itself.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(title: 'Flutter Demo', home: const HomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+/// [Widget] displaying the home page consisting of an image and the buttons.
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+/// State of a [HomePage].
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _urlController = TextEditingController();
+  bool _isMenuOpen = false;
 
-  void _incrementCounter() {
+  void _toggleFullScreen() {
+    if (html.document.fullscreenElement == null) {
+      html.document.documentElement?.requestFullscreen();
+    } else {
+      html.document.exitFullscreen();
+    }
+  }
+
+  void _showMenu() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isMenuOpen = true;
     });
   }
 
+  void _hideMenu() {
+    setState(() {
+      _isMenuOpen = false;
+    });
+  }
+
+  void _loadImage() {
+    html.document.getElementById('imageElement')?.setAttribute('src', _urlController.text);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      appBar: AppBar(),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: GestureDetector(
+                        onDoubleTap: _toggleFullScreen,
+                        child: HtmlElementView(viewType: 'imageElement'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _urlController,
+                        decoration: const InputDecoration(hintText: 'Image URL'),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: _loadImage,
+                      child: const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 12, 0, 12),
+                        child: Icon(Icons.arrow_forward),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 150),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          if (_isMenuOpen)
+            Positioned.fill(
+              child: GestureDetector(
+                onTap: _hideMenu,
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
             ),
-          ],
-        ),
+          Positioned(
+            bottom: 80,
+            right: 16,
+            child: Column(
+              children: [
+                if (_isMenuOpen)
+                  Column(
+                    children: [
+                      FloatingActionButton(
+                        onPressed: () {
+                          _toggleFullScreen();
+                          _hideMenu();
+                        },
+                        child: Icon(Icons.fullscreen),
+                      ),
+                      SizedBox(height: 10),
+                      FloatingActionButton(
+                        onPressed: () {
+                          html.document.exitFullscreen();
+                          _hideMenu();
+                        },
+                        child: Icon(Icons.fullscreen_exit),
+                      ),
+                    ],
+                  ),
+                SizedBox(height: 10),
+                FloatingActionButton(
+                  onPressed: _showMenu,
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
